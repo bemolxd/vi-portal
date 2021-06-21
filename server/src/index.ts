a2afae50d60246283d26711c9366943c436faa90
bemolxd <bartosz.bem@bemideas.pl>
2021-06-21 18:38:24 +0200

    server: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,7 +48,10 @@ const main = async () => {
     }),
     context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: { origin: 'http://localhost:3000' },
+  }); //origin: dev
 
   app.listen(4000, () => {
     console.log('viPortal server running on localhost:4000');
